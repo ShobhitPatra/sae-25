19bc966b940ae250f864413c9d0c1fe167195a33
ShobhitPatra <shobhit9999999@gmail.com>
2025-02-14 14:22:49 +0530

    upcoming

--- components/HeroSecttion.tsx
+++ components/HeroSecttion.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import gear from "public/gear.png";
+import Button from "./ui/Button";
 const HeroSecttion = () => {
   return (
     <section className=" md:pt-56  flex justify-center">
@@ -25,23 +26,7 @@ const HeroSecttion = () => {
           to complement your vision and skills. Start building your dream team
           today
         </p>
-        <button className="bg-gradient-to-r from-[#FF8400] via-[#FFA953] to-[#FF982E] flex justify-center items-center rounded-full py-3 px-7 ">
-          <label className="text-2xl font-medium flex items-center justify-center gap-2 text-black">
-            Explore{" "}
-            <svg
-              width="9"
-              height="17"
-              viewBox="0 0 9 19"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0.309878 0.47278C0.116534 0.728485 0.00992605 1.0737 0.0134981 1.43251C0.01707 1.79132 0.13053 2.13435 0.328926 2.38615L5.56738 9.03283L0.462288 15.7825C0.274509 16.0396 0.172602 16.3824 0.178514 16.7372C0.184427 17.092 0.297686 17.4303 0.493899 17.6792C0.690111 17.9282 0.953578 18.0679 1.22755 18.0683C1.50153 18.0686 1.76409 17.9296 1.95869 17.6811L7.79293 9.96746C7.98627 9.71175 8.09288 9.36653 8.08931 9.00772C8.08573 8.64891 7.97228 8.30589 7.77388 8.05408L1.78723 0.458073C1.58877 0.206344 1.32165 0.06648 1.04461 0.069238C0.767565 0.071996 0.503282 0.21715 0.309878 0.47278Z"
-                fill="#2B2C2C"
-              />
-            </svg>
-          </label>
-        </button>
+        <Button />
       </div>
       {/* gear  */}
       <div>
--- components/homepage/Upcoming.tsx
+++ components/homepage/Upcoming.tsx
@@ -1,15 +1,37 @@
 import Image from "next/image";
 import upcoming_wheel from "public/upcoming_wheel.png";
+import HomeCard from "../ui/HomeCard";
+import upcmng_events from "public/upcmng_events.png";
+import upcmng_saeIndia from "public/upcmng_saeIndia.png";
+import upcmng_projects from "public/upcmng_projects.png";
 
 const Upcoming = () => {
   return (
-    <section className="flex items-center w-full bg-[#2B2C2C]  ">
+    <section className="flex-col items-center w-full bg-[#2B2C2C]  ">
       {/* upcoming heading */}
       <div className="flex md:pl-16">
         <p className="p-1 md:text-7xl font-poppins font-semibold bg-gradient-to-r from-[#FF8400] via-[#FFA953] to-[#FF982E] bg-clip-text text-transparent">
           UPCOMING
         </p>
         <Image src={upcoming_wheel} alt="upcoming-wheel" className="size-20" />
+      </div>
+      {/* panel  */}
+      <div className="flex flex-wrap justify-evenly p-8">
+        <HomeCard
+          image={upcmng_events}
+          title="EVENTS"
+          content="Connect with like-minded entrepreneurs and find the perfect co-founder to complement your vision and skills. Start building your dream team today"
+        />
+        <HomeCard
+          image={upcmng_saeIndia}
+          title="SAE INDIA"
+          content="Connect with like-minded entrepreneurs and find the perfect co-founder to complement your vision and skills. Start building your dream team today    "
+        />
+        <HomeCard
+          image={upcmng_projects}
+          title="PROJECTS"
+          content="Connect with like-minded entrepreneurs and find the perfect co-founder to complement your vision and skills. Start building your dream team today"
+        />
       </div>
     </section>
   );
